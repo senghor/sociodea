586dbc19b30d0e960bf85d03d6fb2f86cf601802
Albert Senghor <asenghor@gmail.com>
2023-10-31 14:29:28 +0300

    Added Post model

---
+++ server/models/Post.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose'
+import { Schema, model } from 'mongoose'
+
+const postSchema = new Schema({
+    userId: {
+        type: String,
+        required: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    location: String,
+    description: String,
+    picturePath: String,
+    userPicturePath: String,
+    likes: {
+        type: Map,
+        of: Boolean
+    },
+    comments: {
+        type: Array,
+        default: [],
+    }
+},{ timestamps: true })
+
+const Post = model(Post, postSchema)
+
+export default Post
