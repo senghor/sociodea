d20dd78fdd080d47e286745facc6fa35ccb66da0
Albert Senghor <asenghor@gmail.com>
2023-11-02 14:19:38 +0300

    Added Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom'
+import HomePage from 'scenes/homePage'
+import LoginPage from 'scenes/loginPage'
+import ProfilePage from 'scenes/profilePage';
 
 function App() {
-  return (
-    <div className="app">
+  return <div className="app">
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<LoginPage />} />
+        <Route path='/home' element={<HomePage />} />
+        <Route path='/profile/:id' element={<ProfilePage />} />
+      </Routes>
+    </BrowserRouter>
     </div>
-  );
+  
 }
-
 export default App;
