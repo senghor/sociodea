6b908a5bf54d2e8c6bc5c6eecfedc79bb168726e
Albert Senghor <asenghor@gmail.com>
2023-11-09 13:35:35 +0300

    Fixed some logic in users controllers

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -5,7 +5,7 @@ export const getUser = async (req, res) => {
     try {
         const { id } = req.params
         const user = await User.findById(id)
-        res.send(200).json(user)
+        res.status(200).json(user)
     } catch (err) {
         res.status(404).json({message: err.message})
     }
@@ -59,5 +59,3 @@ export const addRemoveFriend = async (req, res) => {
 
     }
 }
-
-//continue at 01:10:34
