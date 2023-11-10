ee4800bc4362eb9409de30f1f9dbf0cef8169f29
Albert Senghor <asenghor@gmail.com>
2023-11-10 09:12:37 +0300

    Added Advert and FriendList widgets to homePage.jsx

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -4,6 +4,8 @@ import Navbar from 'scenes/navbar'
 import UserWidget from 'scenes/widgets/UserWidget'
 import MyPostWidget from  'scenes/widgets/MyPostWidget'
 import PostsWidget from 'scenes/widgets/PostsWidget'
+import AdvertWidget from 'scenes/widgets/AdvertWidgets'
+import FriendListWidget from 'scenes/widgets/FriendListWidget'
 const HomePage = () => {
     const isNonMobileScreens = useMediaQuery('(min-width:1000px)')
     const { _id, picturePath } = useSelector((state) => state.user)
@@ -27,6 +29,9 @@ const HomePage = () => {
             </Box>
             {isNonMobileScreens && (
                 <Box flexBasis='26%'>
+                    <AdvertWidget />
+                    <Box margin='2rem 0' />
+                    <FriendListWidget userId={_id} />
                 </Box>
             )}
         </Box>
