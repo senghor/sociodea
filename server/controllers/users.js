c2b618a8d04afee2748c08230dca2cb233fb0caa
Albert Senghor <asenghor@gmail.com>
2023-11-10 09:18:15 +0300

    Optimized getUserFriends() in user controller

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -15,13 +15,13 @@ export const getUserFriends = async (req, res) => {
 try {
     const { id } = req.params
     const user = await User.findById(id)
-     const friends = Promise.all(
-        user.friends.map(id =>  User.findById(id))
-     )
-     const formattedFriends = friends.map(({ _id, firstName, lastName, occupation, location, picturePath }) => {
-        return { _id, firstName, lastName, occupation, location, picturePath }
-     })
-     res.status(200).json(formattedFriends)
+     const friends = []
+     for (let friendId of user.friends){
+        const { _id, firstName, lastName, occupation, location, picturePath } = await User.findById(friendId)
+        friends.push({ _id, firstName, lastName, occupation, location, picturePath })
+     }
+
+     res.status(200).json(friends)
 
     } catch (err) {
         res.status(404).json({message: err.message})
